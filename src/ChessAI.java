53e7308b4cecfafa90c8a741a2cbb0b4871ef272
Eduardo Nonemacher <ernonemacher@outlook.com>
2024-09-07 20:16:34 -0300

    Format ChessAI

--- src/ChessAI.java
+++ src/ChessAI.java
@@ -7,33 +7,36 @@ import java.util.Random;
  */
 public class ChessAI {
     private final int AIplayerNo = 2;
-    private ChessController parent;
-    public ChessAI(ChessController parent){
+    private final ChessController parent;
+
+    public ChessAI(ChessController parent) {
         this.parent = parent;
     }
 
-    private ArrayList<PieceMove> getMoves(){
+    private ArrayList<PieceMove> getMoves() {
         ArrayList<PieceMove> moves = new ArrayList<PieceMove>();
-        for(int chpi = 0; chpi< parent.getChessPieces().size(); chpi++){
+        for (int chpi = 0; chpi < parent.getChessPieces().size(); chpi++) {
             ChessPiece chp = parent.getChessPieces().get(chpi);
-            if(chp.getPlayer() == AIplayerNo) {
-                for (Point move : parent.getPossibleMoves(chp)){
-                    int pf = (AIplayerNo == 2) ? 1 : -1;
+            if (chp.getPlayer() == AIplayerNo) {
+                for (Point move : parent.getPossibleMoves(chp)) {
+                    int pf = 1;
                     int newx = (chp.getX() + move.x * pf);
                     int newy = (chp.getY() + move.y * pf);
 
                     if (newx >= 0 && newy >= 0 && newx <= 8 && newy <= 8) {
                         if (parent.isOccupied(newx, newy) == 0) {
-                            PieceMove m = new PieceMove(chpi,new Point(newx,newy));
-                            m.isGoingToCapture =false;
+                            PieceMove m = new PieceMove(chpi, new Point(newx, newy));
+                            m.isGoingToCapture = false;
                             moves.add(m);
-                            System.out.println("MOVE  [x=" + chp.getX() + ",y=" + chp.getY() +"] to {x=" + newx + ",y=" + newy + "]");
+                            System.out.println("MOVE  [x=" + chp.getX() + ",y=" + chp.getY() + "] to {x=" + newx + ",y="
+                                    + newy + "]");
                         } else if (parent.isOccupied(newx, newy) != AIplayerNo) {
-                            PieceMove m = new PieceMove(chpi,new Point(newx,newy));
+                            PieceMove m = new PieceMove(chpi, new Point(newx, newy));
                             m.isGoingToCapture = true;
-                            m.CapturingPiece = parent.getPieceAt(newx,newy);
+                            m.CapturingPiece = parent.getPieceAt(newx, newy);
                             moves.add(m);
-                            System.out.println("CAPTURE  [x=" + chp.getX() + ",y=" + chp.getY() +"] to {x=" + newx + ",y=" + newy + "]");
+                            System.out.println("CAPTURE  [x=" + chp.getX() + ",y=" + chp.getY() + "] to {x=" + newx
+                                    + ",y=" + newy + "]");
                         }
                     }
                 }
@@ -42,14 +45,14 @@ public class ChessAI {
         return moves;
     }
 
-    public PieceMove playMove(){
+    public PieceMove playMove() {
         Random rnd = new Random();
         ArrayList<PieceMove> moves = getMoves();
         int i = rnd.nextInt(moves.size());
         return moves.get(i);
     }
 
-    public void printMoves(){
+    public void printMoves() {
         System.out.println("Printing Moves ...");
         System.out.println(getMoves().size());
     }
